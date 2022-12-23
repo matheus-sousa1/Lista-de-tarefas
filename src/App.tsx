34c969413695970a837b9f31d5ca4dc8234bd389
matheus sousa <matusalem3378@gmail.com>
2022-12-22 22:38:19 -0300

    commit adicionando funcionamento do botão

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,44 @@
-import React, { FC } from "react";
+import React, { FC, ChangeEvent, useState } from "react";
+import { ITask } from "./Interfaces";
 
 import "./App.css";
 
 const App: FC = () => {
+  const [task, setTask] = useState<string>("");
+  const [deadLine, setDeadLine] = useState<number>(0);
+  const [todoList, setTodoList] = useState<ITask[]>([]);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    if (event.target.name === "task") {
+      setTask(event.target.value);
+    } else {
+      setDeadLine(Number(event.target.value));
+    }
+  };
+
+  const addTask = (): void => {
+    const newTask = { taskName: task, deadLine: deadLine };
+    setTodoList([...todoList, newTask]);
+    console.log(todoList);
+  };
   return (
     <div className="App">
       <div className="header">
         <div className="inputContainer">
-        <input type="text" placeholder="Tarefa" />
-        <input type="number" placeholder="DeadLine (in days)..." />
+          <input
+            type="text"
+            placeholder="Tarefa"
+            name="task"
+            onChange={handleChange}
+          />
+          <input
+            type="number"
+            placeholder="DeadLine (in days)..."
+            name="deadLine"
+            onChange={handleChange}
+          />
         </div>
-        <button>Adicionar Tarefa</button>
+        <button onClick={addTask}>Adicionar Tarefa</button>
       </div>
       <div className="todoList"></div>
     </div>
